dd506717fcb1825b2a416d77db0bcb7ad6082068
岩冰 <1493001032@qq.com>
2024-09-25 15:02:35 +0800

    数据修改

--- internal/controller/department.go
+++ internal/controller/department.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"iot-go/internal/config"
@@ -16,7 +17,48 @@ func RegisterDepartmentRoutes(router *gin.RouterGroup) {
 	router.POST("/create", CreateDepartment)
 	router.GET("/:id", GetById)
 	router.GET("/tree", Tree)
+	router.DELETE("/:id", DeleteById)
 }
+
+// DeleteById 删除
+func DeleteById(c *gin.Context) {
+	idStr := c.Param("id")
+	if idStr == "" {
+		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("未提供 ID"))
+		return
+	}
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("无效的 ID"))
+		return
+	}
+	// 判断是否可以删
+	validError := validateDelete(id)
+	if validError != nil {
+		c.JSON(http.StatusBadRequest, result.Error(validError))
+		return
+	}
+	tx := config.Db.Delete(&model.Department{}, id)
+	if tx.Error != nil {
+		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("删除错误:"+tx.Error.Error()))
+		return
+	}
+	if tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("删除结果为0"))
+		return
+	}
+	c.JSON(http.StatusOK, result.Data("删除成功"))
+}
+
+func validateDelete(id int) error {
+	var exists bool
+	config.Db.Model(&model.Department{}).Where("parent_id = ?", id).Select("count(*) > 0").Scan(&exists)
+	if exists {
+		return fmt.Errorf("存在子级部门, 无法删除")
+	}
+	return nil
+}
+
 func CreateDepartment(c *gin.Context) {
 	db := config.Db
 	d := &model.Department{}
@@ -63,27 +105,25 @@ func Tree(c *gin.Context) {
 	id := c.Query("id")
 	if id == "" {
 		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("参数ID不存在"))
+		return
 	}
-	parseUint, err := strconv.ParseUint(id, 10, 32)
+	parseUint, err := strconv.Atoi(id)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, result.ErrorWithMessage("参数ID为int类型"))
+		return
 	}
-	children, err := getDepartmentWithChildren(config.Db, uint(parseUint))
+	children, erMsg := getDepartmentWithChildren(config.Db, uint(parseUint))
+	if erMsg != nil {
+		c.JSON(http.StatusNotFound, result.ErrorWithMessage("notFount"))
+		return
+	}
 
 	c.JSON(http.StatusOK, result.Data(children))
 }
 func getDepartmentWithChildren(db *gorm.DB, id uint) (*model.Department, error) {
 	var department model.Department
-	if err := db.Preload("Children").First(&department, id).Error; err != nil {
+	if err := db.Preload("Children.Children").First(&department, id).Error; err != nil {
 		return nil, err
 	}
-	for i := range department.Children {
-		child, err := getDepartmentWithChildren(db, department.Children[i].ID)
-		if err != nil {
-			return nil, err
-		}
-		department.Children[i] = *child
-	}
-
 	return &department, nil
 }
--- internal/model/departments.go
+++ internal/model/departments.go
@@ -1,7 +1,7 @@
 package model
 
 type Department struct {
-	ID       uint         `gorm:"primaryKey;autoIncrement"`
+	ID       uint         `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name     string       `gorm:"not null" json:"name"`
 	ParentID *uint        `gorm:"foreignKey:ParentID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"parentId,omitempty"`
 	Parent   *Department  `gorm:"foreignKey:ParentID" json:"parent,omitempty"`
